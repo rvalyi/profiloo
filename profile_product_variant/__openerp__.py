09c8ee41d6bef4a4fb1ccd3329e04b0064bc4b7a
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-02 11:55:58 +0100

    [IMP] add profile for product variant

---
+++ profile_product_variant/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{'name': 'Profile for managing variant',
+ 'version': '0.0.1',
+ 'author': 'Akretion',
+ 'website': 'www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'description': """
+    This module will install basic module needed for a project with variant
+ """,
+ 'depends': [
+     'pricelist_per_product',
+     'product_code_builder',
+     'product_variant_inactive',
+ ],
+ 'data': [
+     'views/product_view.xml',
+ ],
+ 'installable': True,
+ 'application': True,
+ }
+
+
+
